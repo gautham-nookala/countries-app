7795a432f2d89130100b1d3df28247b55cafc91f
Gautham Nookala <gautham.nookala@GauthamNookala-MacBook-Pro-AppleOsX-1331-RYF3.local>
2025-02-28 10:53:43 -0800

    move countrylist out

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import SearchInput from "./Search";
+import CountryListItem from "./CountryList";
 
 // Reusable PageHeader Component
 interface PageHeaderProps {
@@ -30,29 +31,7 @@ const DataTableHeader = () => (
   </div>
 );
 
-// Reusable CountryListItem Component
-interface CountryListItemProps {
-  cca3: string;
-  name: string;
-  continent: string;
-}
 
-const CountryListItem = ({ cca3, name, continent }: CountryListItemProps) => (
-  <Link
-    to={`/country/${cca3}`}
-    className="bg-white rounded-xl shadow-card h-20 hover:shadow-lg transition-shadow duration-300"
-  >
-    <div className="grid grid-cols-12 h-full items-center font-assistant text-base font-semibold text-column-text">
-      <div className="col-span-3 pl-12 flex items-center">
-        <div className="w-12 h-12 rounded-full bg-gray-100"></div>
-      </div>
-
-      <h2 className="col-span-5">{name}</h2>
-
-      <p className="col-span-4">{continent}</p>
-    </div>
-  </Link>
-);
 
 // Reusable LoadingState Component
 const LoadingState = () => (
