649419cd4f6d008d2fc5afcbcbb68814cff80698
Gautham Nookala <gautham.nookala@GauthamNookala-MacBook-Pro-AppleOsX-1331-RYF3.local>
2025-02-28 17:15:56 -0800

    fix responsive padding

--- src/components/CountryListItem.tsx
+++ src/components/CountryListItem.tsx
@@ -12,7 +12,7 @@ const CountryListItem = ({ cca3, name, continent }: CountryListItemProps) => (
     className="bg-white rounded-xl shadow-card h-20 hover:shadow-lg transition-shadow duration-300"
   >
     <div className="grid grid-cols-12 h-full items-center font-assistant text-base font-semibold text-column-text">
-      <div className="col-span-3 pl-12 flex items-center">
+      <div className="col-span-3 pl-6 md:pl-12 flex items-center">
         <div className="w-12 h-12 rounded-full bg-gray-100"></div>
       </div>
 
